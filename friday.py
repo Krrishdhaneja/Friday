713ed78e7d3b46c20c39c84dc60f7277af5536c0
Krrish Dhaneja <krrish21march@gmail.com>
2020-10-30 20:33:31 +0530

    Added Some cool new features. Check it out

--- friday.py
+++ friday.py
@@ -1,3 +1,4 @@
+from googlesearch import *
 import pyttsx3
 import webbrowser
 import random
@@ -10,15 +11,16 @@ import sys
 
 engine = pyttsx3.init('sapi5')
 
-client = wolframalpha.Client('your_client_id')
-
+client = wolframalpha.Client('EV3UUJ-ET9UTALAH3')
 voices = engine.getProperty('voices')
-engine.setProperty('voice' ,voices[0].id)
+engine.setProperty('voice', voices[0].id)
+
 
 def speak(audio):
     print('Computer: ' + audio)
     engine.say(audio)
     engine.runAndWait()
+
 
 def greetMe():
     currentH = int(datetime.datetime.now().hour)
@@ -28,27 +30,29 @@ def greetMe():
     if currentH >= 12 and currentH < 18:
         speak('Good Afternoon! , sir')
 
-    if currentH >= 18 and currentH !=0:
+    if currentH >= 18 and currentH != 0:
         speak('Good Evening! , sir')
+
 
 greetMe()
 
-#speak("what\'s the password")
-#k=str(input("password:"))
-#if "your_password" in k:
-#    speak("welcome back ,  sir")
-#else:
-#    sys.exit()
+speak("what\'s the password")
+k = str(input("password:"))
+if "just a rather very intelligent system" in k:
+    speak("welcome back ,  sir")
+    speak('what can I do for you ,  sir')
+else:
+    sys.exit()
 
 
-speak('what can i do for you , sir')
+#speak('what can i do for you , sir')
 
 def myCommand():
 
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
-        r.pause_threshold =  0.3
+        r.pause_threshold = 1
         audio = r.listen(source)
     try:
         query = r.recognize_google(audio, language='en-in')
@@ -68,41 +72,58 @@ if __name__ == '__main__':
         query = myCommand()
         query = query.lower()
 
-        if 'open youtube' in query:
+        if 'open youtube' in query or 'open YouTube' in query:
             speak('okay')
             webbrowser.open('www.youtube.com')
 
         elif 'open google' in query:
             speak('okay')
-            webbrowser.open('www.google.co.in')
+            webbrowser.open('www.google.com')
 
         elif 'open gmail' in query:
             speak('okay')
             webbrowser.open('www.gmail.com')
+        
+        elif 'open github' in query:
+            speak('okay')
+            webbrowser.open('www.github.com')
 
-        elif "what\'s up" in query or 'how are you' in query:
-            stMsgs = ['Just doing my thing!', 'I am fine!', 'Nice!', 'I am nice and full of energy']
+        elif "what\'s up" in query or 'how are you friday' in query:
+            stMsgs = ['Just doing my thing!', 'I am fine!',
+                      'Nice!', 'I am nice and full of energy']
             speak(random.choice(stMsgs))
 
-        elif "do calculations" in query:
+        elif "hey friday" in query:
+            # speak(greetMe())
+            k = ["what can i do for you , sir", "how can i help you"]
+            speak(random.choice(k))
+
+        elif 'search' in query or 'do a search' in query:
+            speak('what should I search for ?  sir')
+            ab = myCommand()
+            chrome_path = r'C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe %s'
+            for url in search(ab, tld="co.in", num=1, stop=1, pause=2):
+                webbrowser.open("https://google.com/search?q=%s" % ab)
+            speak('done , sir')
+
+        elif "do calculations" in query or 'do some calculations' in query:
             speak("ok , sir")
             speak("which is the first number")
-            d=int(input("first number:"))
+            d = myCommand()
+            d1 = float(d)
             speak("which is the second number")
-            t=int(input("second number:"))
-            speak("and which operator")
-            k=str(input("operator:"))
-            if "+" in k:
-              print(d+t)
-            if "-" in k:
-              print(d-t)
-            if "x" in k:
-              print(d*t)
-            if "/" in k:
-              print(d/t)
-            if "^" in k:
-              print(d**t)
-
+            t = myCommand()
+            t1 = float(t)
+            speak('which operator')
+            q = myCommand()
+            if 'minus' in q or 'Minus' in q or '-' in q:
+                print(d1-t1)
+            elif 'plus' in q or 'Plus' in q or '+' in q:
+                print(d1+t1)
+            elif 'multiply' in q or 'Multiply' or 'x' in q or '*' in q:
+                print(d1*t1)
+            elif 'divide' in q or 'Divide' in q or '/' in q:
+                print(d1/t1)
 
         elif 'nothing' in query or 'abort' in query or 'stop' in query or 'no' in query:
             speak('okay')
@@ -116,25 +137,22 @@ if __name__ == '__main__':
             speak('Bye Sir, have a good day.')
             sys.exit()
 
-        elif "change your voice" in query:
+        elif 'change your voice' in query:
             os.startfile('jarvis.py')
-            sys.exit
+            sys.exit()
 
         elif "make a list" in query:
             speak("ok sir")
-            k = str(input("what is the item:"))
+            p = str(input("what is the item:"))
             speak("things added")
 
         elif "add items in my list" in query:
             speak("ok sir")
-            d=str(input("what is the new item:"))
+            d = str(input("what is the new item:"))
             speak("things added")
 
-        elif "what\'s in my list" in query or "what is in my list" in query:
-            speak("now telling list items")
-
-
-
+       # elif "what\'s in my list" in query or "what is in my list" in query:
+          #  speak("now telling list items")
 
         else:
             query = query
