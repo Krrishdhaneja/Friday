7886e6a89de1e3b8395a5ae43f05f0b7d6cc5cea
Krrish Dhaneja <krrish21march@gmail.com>
2020-11-03 13:01:35 +0530

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='virtual-assistant',
+    version='v1.3',
+    packages=['pyttsx3', 'wolframalpha', 'wikipedia', 'speechRecognition', 'datetime', 'beautifulsoup4', 'google'],
+    url='https://github.com/Krrishdhaneja/virtual-assistant',
+    license='GPL v3',
+    author='Krrish Dhaneja',
+    author_email='krrish21march@gmail.com',
+    description='A virtual assistant made in python.'
+)
