293be54d547bd69b9a83939dae9904c1348306e5
Florian <64581222+fm-sys@users.noreply.github.com>
2020-12-29 18:21:45 +0100

    make travis-ci happy (part 2) ;-)

---
+++ test_example.py
@@ -0,0 +1,6 @@
+def test_dummyUnitTest():
+    # without any unit tests the ci is failing
+    # unit tests should be used later on at higher code complexity
+    # they are used to verify that core functionalities are not damaged by code changes
+    assert 2 + 3 == 5
+
